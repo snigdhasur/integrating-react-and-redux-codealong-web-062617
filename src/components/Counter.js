6b570d77e491f238338d158242fd9b4607695852
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-09-15 09:52:21 -0400

    modified file

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
-export default (props) => {
-  return (
-    <div>
-      Counter Component
-    </div>
-  )
-};
+  import React from 'react'
+ 
+  export default (props) => {
+ 
+    const handleOnClick = () => {
+      props.store.dispatch({ type: 'INCREASE_COUNT' });
+    }
+ 
+    return (
+      <div>
+        <button onClick={handleOnClick}>
+          Click Me
+        </button>
+        <div>{props.store}</div>
+      </div>
+    )
+  };
+ 
--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,21 @@
+export default function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    console.log(`the state is ${state.count}`);
+    console.log(`the action is ${action.type}`);
+    // render();
+  };
+
+  function getState(){
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+};
+
 
